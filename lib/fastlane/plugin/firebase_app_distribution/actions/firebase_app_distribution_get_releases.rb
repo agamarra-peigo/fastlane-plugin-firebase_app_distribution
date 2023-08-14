6be1dedd586301ddbe327f7e5822c30388a5a825
Arturo Gamarra <agamarra@peigo.com.ec>
2023-08-14 10:15:39 -0500

    [Techtask] Filter releleases based on version

--- lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_get_releases.rb
+++ lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_get_releases.rb
@@ -34,7 +34,7 @@ module Fastlane
           latest_release = nil
           UI.important("No releases for app #{params[:app]} found in App Distribution. Returning nil and setting Actions.lane_context[SharedValues::FIREBASE_APP_DISTRO_LATEST_RELEASE].")
         else
-          releases = releases.map {|rel| map_release_hash(rel) if rel.display_version == version }.compact
+          releases = releases.map {|rel| map_release_hash(rel)  }.compact
           UI.success("✅ Latest release fetched successfully. Returning release and setting Actions.lane_context[SharedValues::FIREBASE_APP_DISTRO_LATEST_RELEASE].")
         end
         Actions.lane_context[SharedValues::FIREBASE_APP_DISTRO_LATEST_RELEASE] = latest_release
